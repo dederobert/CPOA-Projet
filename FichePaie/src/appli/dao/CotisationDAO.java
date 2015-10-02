26fa4a4c22ff8298b2257de9fb33e822013e148e
MonkeyDDeadpool <al.lajoux@laposte.net>
2015-10-02 08:15:23 +0200

    YOLO

--- FichePaie/src/appli/dao/CotisationDAO.java
+++ FichePaie/src/appli/dao/CotisationDAO.java
@@ -4,7 +4,7 @@ import appli.modele.metier.Cotisation;
 
 /**
  * Interface de la gestion de persistance des Cotisation <br /> <b>DAO niveau 2</b>
- * @author alexis
+ * @author alexis & alexis
  *
  */
 public interface CotisationDAO extends DAO<Cotisation>{
