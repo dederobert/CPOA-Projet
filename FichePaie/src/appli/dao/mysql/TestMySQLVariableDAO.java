62cef3640521b58d05e520a3e12926402e33fd7c
lajoux2u <lajoux2u@iutm-inf-f36-13.iutmetz.ad.univ-lorraine.fr>
2015-10-09 11:16:24 +0200

    rolls royce Petasse

---
+++ FichePaie/src/appli/dao/mysql/TestMySQLVariableDAO.java
@@ -0,0 +1,66 @@
+package appli.dao.mysql;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import appli.modele.metier.Variable;
+
+public class TestMySQLVariableDAO {
+
+	@Test
+	public void testSingleton() {
+		assertNotNull(MySQLVariableDAO.getInstance());
+	}
+	
+	@Test
+	public void testCreerVariable(){
+		Variable variable = new Variable("Brute");
+		int id = MySQLVariableDAO.getInstance().create(variable);
+		Variable variable2 = MySQLVariableDAO.getInstance().getById(id);		
+		assertEquals("Brute", variable2.getLibelle());				
+	}
+
+	@Test
+	public void testCreerVariableNotNull(){
+		Variable variable = new Variable("Brute");
+		int id = MySQLVariableDAO.getInstance().create(variable);
+		assertNotNull(id);
+	}
+	
+	@Test
+	public void testGetByIdVariableNotNull(){
+		assertNotNull(MySQLVariableDAO.getInstance().getById(1));
+	}
+	
+	@Test
+	public void testGetByIdVariable(){
+		assertEquals(new Variable("Statu"),MySQLVariableDAO.getInstance().getById(1));
+	}
+	
+	@Test
+	public void testGetByLibelleNotNull(){
+		assertNotNull(MySQLVariableDAO.getInstance().getByLibelle("Brute"));
+	}
+	
+	@Test
+	public void testGetByLibelleIsSame(){
+		assertEquals(new Variable("Statu"), MySQLVariableDAO.getInstance().getByLibelle("Statu"));
+	}
+	
+	@Test
+	public void testUpdateVariable(){
+		Variable variable = MySQLVariableDAO.getInstance().getById(1);
+		variable.setLibelle("testUpdate");
+		MySQLVariableDAO.getInstance().update(variable);
+		assertEquals(variable.getLibelle(), MySQLVariableDAO.getInstance().getById(1).getLibelle());
+	}
+	
+	@Test
+	public void testDelete(){
+		Variable variable = new Variable("Brute");
+		int id = MySQLVariableDAO.getInstance().create(variable);
+		MySQLVariableDAO.getInstance().delete(variable);
+		assertNull(MySQLVariableDAO.getInstance().getById(id));
+	}
+}
