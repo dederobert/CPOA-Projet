86d87436987271c2d22c8c2fc1fc942e1a2afc07
Dederobert <dederoberthd@gmail.com>
2015-10-13 18:52:53 +0200

    JavaDoc de Employe

--- FichePaie/src/appli/modele/metier/Employe.java
+++ FichePaie/src/appli/modele/metier/Employe.java
@@ -1,30 +1,82 @@
 package appli.modele.metier;
 
+import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Class metier, Employe <br>
+ * Un employé: un nom, un prenom, une adresse,
+ *  une liste de regles et de variable associé à une valeur
+ * @author alexis
+ * @version 1.0
+ */
 public class Employe {
 
+		/**
+		 * Identifiant de l'employe
+		 */
         private int id;
+        /**
+         * Nom de l'employe
+         */
         private String nom;
+        /**
+         * Prenom de l'employe
+         */
         private String prenom;
+        /**
+         * Adresse de l'employe
+         */
         private String adresse;
+        /**
+         * Liste des regles que possèdent un employé
+         */
         private ArrayList<Regle> regles = null;
+        /**
+         * Tableau associatif variable/valeur que possède un employé
+         */
         private HashMap<Variable, String> variables = null;
 
+        /**
+         * Constrcteur sans paramètre de {@link Employe}<br>
+         * id -1, nom "", prenom "", adresse "", regles emptyList, variables emptyList
+         */
         public Employe() {
                 this(-1, "", "", "");
         }
 
+        /**
+         * Constructeur de {@link Employe}
+         * @param nom Nom de l'employé
+         * @param prenom prenom de l'employé
+         * @param adresse adresse de l'employé
+         */
         public Employe(String nom, String prenom, String adresse) {
                 this(-1, nom, prenom, adresse);
         }
 
+        /**
+         * Constrcuteur de {@link Employe}
+         * @param id Identifiant de l'employé
+         * @param nom Nom de l'employé
+         * @param prenom prenom de l'employé
+         * @param adresse adresse de l'employé
+         */
         public Employe(int id, String nom, String prenom, String adresse) {
                 this(id, nom, prenom, adresse, new ArrayList<Regle>(), new HashMap<Variable, String>());
         }
         
 
+        /**
+         * Constrcuteur de {@link Employe}
+         * @param id Identifiant de l'employé
+         * @param nom Nom de l'employé
+         * @param prenom prenom de l'employé
+         * @param adresse adresse de l'employé
+         * @param regles Regles que possèdent l'employé
+         * @param possede Variable/valeur associé à l'employé
+         */
         public Employe(int id, String nom, String prenom, String adresse,
 				ArrayList<Regle> regles, HashMap<Variable, String> possede) {
         	this.setId(id);
@@ -35,6 +87,10 @@ public class Employe {
             this.setVariables(possede);
 		}
 
+        /**
+         * {@link GetField}
+         * @return
+         */
 		public int getId() {
                 return id;
         }
@@ -43,6 +99,10 @@ public class Employe {
                 this.id = id;
         }
 
+        /**
+         * {@link GetField}
+         * @return
+         */
         public String getNom() {
                 return nom;
         }
@@ -51,6 +111,10 @@ public class Employe {
                 this.nom = nom;
         }
 
+        /**
+         * {@link GetField}
+         * @return
+         */
         public String getPrenom() {
                 return prenom;
         }
@@ -59,6 +123,10 @@ public class Employe {
                 this.prenom = prenom;
         }
 
+        /**
+         * {@link GetField}
+         * @return
+         */
         public String getAdresse() {
                 return adresse;
         }
@@ -67,6 +135,10 @@ public class Employe {
                 this.adresse = adresse;
         }
 
+        /**
+         * {@link GetField}
+         * @return
+         */
 		public ArrayList<Regle> getRegles() {
 			return regles;
 		}
@@ -75,6 +147,10 @@ public class Employe {
 			this.regles = regles;
 		}
 
+		 /**
+         * {@link GetField}
+         * @return
+         */
 		public HashMap<Variable, String> getVariables() {
 			return variables;
 		}
